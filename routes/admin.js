ad22c83a9bd411e07096c2735508296e0c625b62
ramimamdouhali <31680248+ramimamdouhali@users.noreply.github.com>
2026-05-20 00:29:52 +0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,1 +1,28 @@
+const {
+    renderPage
+} = require('../helpers/render');
 
+const {
+    getPageConfig,
+    checkAuth
+} = require('../helpers/auth');
+
+const {
+    getPageData,
+    savePrices,
+    getDB
+} = require('../services/database');
+
+async function handleAdminPanel(
+    req,
+    res,
+    pageId,
+    MASTER_PASSWORD,
+    url
+) {
+
+}
+
+module.exports = {
+    handleAdminPanel
+};
