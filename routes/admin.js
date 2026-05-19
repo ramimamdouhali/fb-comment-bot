7fa6a03defe13c407a5579d2dd8203602482535e
ramimamdouhali <31680248+ramimamdouhali@users.noreply.github.com>
2026-05-20 01:03:43 +0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -290,6 +290,102 @@ async function handleAdminPanel(
 
 //}
 
+
+
+
+    function handleAddProduct(
+        req,
+        res,
+        pageId,
+        MASTER_PASSWORD
+    ) {
+    
+        const config =
+            getPageConfig(pageId);
+    
+        if (
+            !checkAuth(
+                req,
+                config.password,
+                MASTER_PASSWORD
+            )
+        ) {
+    
+            res.writeHead(401, {
+                'WWW-Authenticate':
+                    'Basic realm="Admin Panel"'
+            });
+    
+            res.end('Unauthorized');
+    
+            return;
+        }
+    
+        let body = '';
+    
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+    
+        req.on('end', async () => {
+    
+            const params =
+                new URLSearchParams(body);
+    
+            const code =
+                params.get('code');
+    
+            const price =
+                parseFloat(
+                    params.get('price')
+                );
+    
+            const pageData =
+                await getPageData(pageId);
+    
+            const prices =
+                pageData?.prices || {};
+    
+            if (prices[code]) {
+    
+                res.writeHead(302, {
+                    Location:
+                        `/admin/${pageId}?error=duplicate`
+                });
+    
+                res.end();
+    
+                return;
+            }
+    
+            prices[code] = price;
+    
+            await savePrices(
+                pageId,
+                prices
+            );
+    
+            res.writeHead(302, {
+                Location:
+                    `/admin/${pageId}?success=added`
+            });
+    
+            res.end();
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = {
-    handleAdminPanel
+    handleAdminPanel,
+    handleAddProduct
 };
