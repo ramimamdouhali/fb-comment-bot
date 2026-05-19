70fb2040343d367bd656617c25954799e67489b9
ramimamdouhali <31680248+ramimamdouhali@users.noreply.github.com>
2026-05-19 17:47:33 +0300

    Create database.js

---
+++ services/database.js
@@ -0,0 +1,59 @@
+const { MongoClient } =
+    require('mongodb');
+let db;
+function getDB() {
+    return db;
+}
+
+async function connectDB() {
+    const client = new MongoClient(MONGODB_URI);
+    await client.connect();
+    db = client.db();
+    console.log("Connected to MongoDB");
+}
+
+async function getPageData(pageId) {
+    if (!db) return null;
+    return await db.collection('pages').findOne({ pageId });
+}
+
+async function savePrices(
+    pageId,
+    prices
+) {
+
+    if (!db) return false;
+
+    await db
+        .collection('pages')
+        .updateOne(
+            { pageId },
+
+            {
+                $set: {
+                    prices
+                },
+
+                $setOnInsert: {
+
+                    publicReplies: [
+                        'Thanks for your comment!'
+                    ],
+
+                    subscriptionExpiry:
+                        new Date()
+                }
+            },
+
+            { upsert: true }
+        );
+
+    return true;
+}
+
+module.exports = {
+    connectDB,
+    getDB,
+    getPageData,
+    savePrices
+};
